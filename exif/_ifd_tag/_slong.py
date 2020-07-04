7fff4c05bc50f824ba4ffb39216b563e5a483313
Tyler Thieding <tyler@thieding.com>
2020-07-04 13:56:53 -0400

    Omit SLONG implementation since nothing uses it.

--- exif/_ifd_tag/_slong.py
+++ exif/_ifd_tag/_slong.py
@@ -1,7 +1,5 @@
 """IFD SLONG tag structure parser module."""
 
-import struct
-
 from exif._ifd_tag._base import Base as BaseIfdTag
 
 
@@ -16,26 +14,15 @@ class Slong(BaseIfdTag):
         :type value: corresponding Python type
 
         """
-        raise NotImplementedError("this package does not yet support setting SLONG tags")
+        raise NotImplementedError("this package does not yet support setting SLONG tags since no SLONG tags "
+                                  "exist in EXIF specification")
 
     def read(self):  # pragma: no cover
         """Read tag value.
-
-        .. note:: No test coverage since no SLONG tags exist in EXIF specifications.
 
         :returns: tag value
         :rtype: corresponding Python type
 
         """
-        retvals = []
-
-        for member_index in range(self.count):  # pylint: disable=unused-variable
-            value_bits = struct.pack('>I', self.value_offset)
-            retvals.append(struct.unpack('>l', value_bits)[self.struct_index])
-
-        if len(retvals) == 1:
-            retval = retvals[0]
-        else:
-            retval = tuple(retvals)
-
-        return retval
+        raise NotImplementedError("this package does not yet support setting SLONG tags since no SLONG tags "
+                                  "exist in EXIF specification")
