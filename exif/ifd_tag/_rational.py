95855ebac7d992daccb64bbd57537a310910f2a3
Tyler Thieding <tyler@thieding.com>
2020-07-04 16:01:44 -0400

    All reads are working!

--- exif/ifd_tag/_rational.py
+++ exif/ifd_tag/_rational.py
@@ -4,13 +4,36 @@ import binascii
 import struct
 from fractions import Fraction
 
+from plum.int.big import UInt32
+from plum.int.little import UInt32 as UInt32_L
+from plum.structure import Member, Structure
+
 from exif._constants import EXIF_LITTLE_ENDIAN_HEADER
+from exif._datatypes import TiffByteOrder
 from exif.ifd_tag._base import Base as BaseIfdTag
+
+
+class RationalDtype(Structure):
+    numerator: int = Member(cls=UInt32)
+    denominator: int = Member(cls=UInt32)
+
+
+class RationalDtype_L(Structure):
+    numerator: int = Member(cls=UInt32_L)
+    denominator: int = Member(cls=UInt32_L)
 
 
 class Rational(BaseIfdTag):
 
     """IFD RATIONAL tag structure parser class."""
+
+    def __init__(self, tag_offset, app1_ref):
+        super().__init__(tag_offset, app1_ref)
+
+        if self._app1_ref.endianness == TiffByteOrder.BIG:
+            self.rational_dtype_cls = RationalDtype
+        else:
+            self.rational_dtype_cls = RationalDtype_L
 
     def modify(self, value):
         """Modify tag value.
@@ -49,17 +72,11 @@ class Rational(BaseIfdTag):
 
         """
         retvals = []
-        cursor = 0xA + self.value_offset
 
-        for member_index in range(self.count):  # pylint: disable=unused-variable
-            data_format = ">LL"
-            numerator, denominator = struct.unpack(data_format, binascii.unhexlify(
-                self.parent_segment_hex.read(cursor, 8)))
-            cursor += 8
-            if self.parent_segment_hex.endianness == EXIF_LITTLE_ENDIAN_HEADER:
-                retvals.append(float(denominator) / float(numerator))
-            else:
-                retvals.append(float(numerator) / float(denominator))
+        for rational_index in range(self._tag_view.value_count.get()):
+            current_offset = self._tag_view.value_offset.get() + rational_index * self.rational_dtype_cls.nbytes
+            rational_view = self.rational_dtype_cls.view(self._app1_ref.body_bytes, current_offset)
+            retvals.append(rational_view.numerator / rational_view.denominator)
 
         if len(retvals) == 1:
             retval = retvals[0]
--- exif/ifd_tag/_srational.py
+++ exif/ifd_tag/_srational.py
@@ -4,16 +4,42 @@ import binascii
 import struct
 from fractions import Fraction
 
+from plum.int.big import SInt32
+from plum.int.little import SInt32 as SInt32_L
+from plum.structure import Member, Structure
+
 from exif._constants import EXIF_LITTLE_ENDIAN_HEADER
+from exif._datatypes import TiffByteOrder
 from exif.ifd_tag._base import Base as BaseIfdTag
+
+
+class SrationalDtype(Structure):
+    numerator: int = Member(cls=SInt32)
+    denominator: int = Member(cls=SInt32)
+
+
+class SrationalDtype_L(Structure):
+    numerator: int = Member(cls=SInt32_L)
+    denominator: int = Member(cls=SInt32_L)
 
 
 class Srational(BaseIfdTag):
 
     """IFD SRATIONAL tag structure parser class."""
 
+    def __init__(self, tag_offset, app1_ref):
+        super().__init__(tag_offset, app1_ref)
+
+        if self._app1_ref.endianness == TiffByteOrder.BIG:
+            self.srational_dtype_cls = SrationalDtype
+        else:
+            self.srational_dtype_cls = SrationalDtype_L
+
     def modify(self, value):
         """Modify tag value.
+
+        This method does not contain logic for unpacking multiple values since the EXIF standard (v2.2) does not list
+        any IFD tags of SRATIONAL type with a count greater than 1.
 
         :param value: new tag value
         :type value: corresponding Python type
@@ -44,26 +70,12 @@ class Srational(BaseIfdTag):
     def read(self):
         """Read tag value.
 
+        This method does not contain logic for unpacking multiple values since the EXIF standard (v2.2) does not list
+        any IFD tags of SRATIONAL type with a count greater than 1.
+
         :returns: tag value
         :rtype: corresponding Python type
 
         """
-        retvals = []
-        cursor = 0xA + self.value_offset
-
-        for member_index in range(self.count):  # pylint: disable=unused-variable
-            data_format = ">ll"
-            numerator, denominator = struct.unpack(data_format, binascii.unhexlify(
-                self.parent_segment_hex.read(cursor, 8)))
-            cursor += 8
-            if self.parent_segment_hex.endianness == EXIF_LITTLE_ENDIAN_HEADER:
-                retvals.append(float(denominator) / float(numerator))
-            else:
-                retvals.append(float(numerator) / float(denominator))
-
-        if len(retvals) == 1:
-            retval = retvals[0]
-        else:
-            retval = tuple(retvals)
-
-        return retval
+        srational_view = self.srational_dtype_cls.view(self._app1_ref.body_bytes, self._tag_view.value_offset.get())
+        return srational_view.numerator / srational_view.denominator
