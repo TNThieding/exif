b85846a11da7bcab00e89b87b0f41583e42da6d4
Tyler Thieding <tyler@thieding.com>
2020-07-11 15:55:50 +0000

    [1.0.0] Support adding tags and adding EXIF to non-EXIF images.

--- exif/__init__.py
+++ exif/__init__.py
@@ -1,7 +1,9 @@
 """Read and modify image EXIF metadata using Python."""
 
 from exif._constants import (
-    ColorSpace, ExposureMode, ExposureProgram, LightSource, MeteringMode, Orientation, ResolutionUnit, Saturation,
-    SceneCaptureType, SensingMethod, Sharpness, WhiteBalance)
+    ColorSpace, ExposureMode, ExposureProgram, GpsAltitudeRef, LightSource, MeteringMode, Orientation, ResolutionUnit,
+    Saturation, SceneCaptureType, SensingMethod, Sharpness, WhiteBalance)
 from exif._datatypes import Flash, FlashMode, FlashReturn
 from exif._image import Image
+
+DATETIME_STR_FORMAT = "%Y:%m:%d %H:%M:%S"
---
+++ exif/_add_tag_utils.py
@@ -0,0 +1,23 @@
+"""Utility functions used when adding tags to an image's APP1 segment."""
+
+from exif._constants import ATTRIBUTE_ID_MAP
+
+
+def value_fits_in_ifd_tag(tag_dt, exif_type_cls):
+    """Determine if value fits inside the IFD tag itself.
+
+    :param Structure tag_dt: IFD tag datatype instance
+    :param Enum exif_type_cls: EXIF type enumeration
+    :returns: value fits inside IFD tag
+    :rtype: bool
+
+    """
+    is_value_in_ifd_tag_itself = tag_dt.type == exif_type_cls.ASCII and tag_dt.value_count <= 4
+    is_value_in_ifd_tag_itself |= tag_dt.type == exif_type_cls.BYTE
+    is_value_in_ifd_tag_itself |= tag_dt.type == exif_type_cls.SHORT
+    is_value_in_ifd_tag_itself |= tag_dt.type == exif_type_cls.LONG
+    is_value_in_ifd_tag_itself |= tag_dt.type == exif_type_cls.SLONG
+    is_value_in_ifd_tag_itself |= tag_dt.tag_id == ATTRIBUTE_ID_MAP["exif_version"]
+    is_value_in_ifd_tag_itself |= tag_dt.tag_id == ATTRIBUTE_ID_MAP["flashpix_version"]
+
+    return is_value_in_ifd_tag_itself
